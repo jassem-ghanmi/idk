bcecb8cd61b11c4decde6dfaa085719479772800
jassem-ghanmi <Jassemghanmi123456789@gmail.com>
2021-09-19 09:55:17 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import {Form,Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to <b>Globex Corporation</b></h1>
+     <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" /> 
+   </Form.Group>
+  <Button href="http://www.5z8.info/whitepower_salq" variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    </div>
+  );
+}
+
+export default App;
